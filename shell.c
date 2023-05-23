0c70abb53282c91dbf6a824c6ade8a16a124bdd9
nikchichua <nik.chichua@gmail.com>
2023-05-22 21:01:03 -0400

    Added the shell loop and signal redirection.

--- shell.c
+++ shell.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <unistd.h>
+#include <pwd.h>
+
+#define MAX_INPUT_LENGTH (65536)
+
+sigjmp_buf jump;
+
+void signal_redirect(void *signal_handler) {
+    struct sigaction signal_action;
+    signal_action.sa_handler = signal_handler;
+    sigemptyset(&signal_action.sa_mask);
+    sigaction(SIGINT, &signal_action, NULL);
+    sigaction(SIGTSTP, &signal_action, NULL);
+}
+
+void signal_handler(int signal) {
+    printf("\n");
+    siglongjmp(jump, 1);
+}
+
+void execute_command(char *input) {
+    printf("%s", input);
+}
+
+int run_shell() {
+    while (true) {
+        char cwd[1024];
+        getcwd(cwd, sizeof(cwd));
+        printf("%s> ", cwd);
+        char input[MAX_INPUT_LENGTH];
+        if (sigsetjmp(jump, 1) != 0) continue;
+        fgets(input, MAX_INPUT_LENGTH, stdin);
+        if(strcasecmp(input, "\n") == 0) continue;
+        // command execution logic goes here!
+        execute_command(input);
+        if (strcasecmp(input, "exit\n") == 0) return 0;
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    signal_redirect(signal_handler);
+    return run_shell();
 }
